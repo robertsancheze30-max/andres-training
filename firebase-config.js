457e61427e7b576da20499fa0bc7c5b832c62d4c
robertsancheze30-max <robertsancheze30@gmail.com>
2026-09-13 15:18:14 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Reemplaza estos valores con los de tu proyecto Firebase "andres-training"
+// (Firebase console -> Configuración del proyecto -> Tus apps -> SDK setup)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "andres-training.firebaseapp.com",
+  databaseURL: "https://andres-training-default-rtdb.firebaseio.com",
+  projectId: "andres-training",
+  storageBucket: "andres-training.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
